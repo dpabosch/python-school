0d3624d2269edb1e23128e4992a5569fefeab5da
Oliver Eickmeyer <oliver.eickmeyer@postadress.de>
2018-08-20 15:05:54 +0200

    unittest eingebaut

--- muster/test_csv.py
+++ muster/test_csv.py
@@ -3,8 +3,9 @@ import muster.commons
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_read_csv(self):
+        list_of_dicts = muster.commons.read_csv('probieren.csv', '../')
+        self.assertNotEqual(list_of_dicts, None)
 
 
 if __name__ == '__main__':
