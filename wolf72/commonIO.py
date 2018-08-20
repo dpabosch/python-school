161e59075714c687a4a23b1ad331100b71ad59ed
Karsten Wolf <karsten.wolf@postadress.de>
2018-08-20 14:56:52 +0200

    Auto stash before rebase of "origin/master"

--- wolf72/commonIO.py
+++ wolf72/commonIO.py
@@ -1,20 +1,18 @@
 import csv
-with open('../probieren.csv', newline='') as csvfile:
-    csvData = csv.DictReader(csvfile, delimiter=' ', quotechar='|')
-    print(csvData.fieldnames)
-    for row in csvData:
-        print(row)
+import os.path
 
-def readCSV(filename, path='./', encoding='UTF-8', delimiter=''):
+def readCSV(filename, path='./', encoding='UTF-8', delimiter=';'):
     contentList = []
     try:
-        with open(path+filename, encoding=encoding) as csvfile:
+        with open(os.path.join(path, filename), encoding=encoding) as csvfile:
             csvData = csv.DictReader(csvfile, delimiter)
-        for row in csvData:
-            contentList.append(row)
+            for row in csvData:
+                contentList.append(row)
     except FileNotFoundError:
         print('Datei konnte nicht gefunden werden.')
     return contentList
 
 
-contentList = readCSV('probieren.csv')
+contentList = readCSV('probieren.csv', '../')
+print(contentList)
+
