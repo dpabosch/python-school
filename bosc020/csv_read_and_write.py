34380a442258c9e35cec412e62d156b271787603
Michel Bosch <michel.bosch@postadress.de>
2018-08-20 11:15:53 +0200

    CSVWriter

---
+++ bosc020/csv_read_and_write.py
@@ -0,0 +1,17 @@
+import csv;
+
+csvReader = csv.DictReader(open('../probieren.csv', newline='', encoding='utf-8'), delimiter=';', quotechar='|')
+
+list_of_entrys = []
+
+
+csvfile=open("output.csv", "w", encoding='utf-8', newline='')
+writer = csv.DictWriter(csvfile, fieldnames=csvReader.fieldnames, delimiter=',')
+writer.writeheader()
+
+for row in csvReader:
+    print(row)
+    list_of_entrys.append(row)
+    writer.writerow(row)
+
+
