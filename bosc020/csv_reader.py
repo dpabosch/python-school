2b005c7377b9e9604aa16c98230c0acc5cc87ab2
Michel Bosch <michel.bosch@postadress.de>
2018-08-20 10:34:31 +0200

    Pathfix

--- bosc020/csv_reader.py
+++ bosc020/csv_reader.py
@@ -1,7 +1,8 @@
 import csv;
 
-spamReader = csv.DictReader(open('probieren.csv', newline='', encoding='utf-8'), delimiter=';', quotechar='|')
+spamReader = csv.DictReader(open('../probieren.csv', newline='', encoding='utf-8'), delimiter=';', quotechar='|')
 
 list_of_entrys = []
 for row in spamReader:
+    print(row)
     list_of_entrys.append(row)
