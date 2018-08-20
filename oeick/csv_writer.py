a6762a7568221c4ca2f2a4c15dcdfd0defa7917d
Oliver Eickmeyer <oliver.eickmeyer@postadress.de>
2018-08-20 11:41:17 +0200

    nochmal Indentation verschoenert

--- oeick/csv_writer.py
+++ oeick/csv_writer.py
@@ -2,9 +2,10 @@
 
 import csv
 
-reader = csv.DictReader(
-           open('../probieren.csv', 'r', encoding='utf-8'),
-           delimiter=';')
+reader = csv.DictReader(open('../probieren.csv',
+                             'r',
+                             encoding='utf-8'),
+                        delimiter=';')
 
 simple_dict = [{f: r[f] for f in r} for r in reader]
 print(simple_dict)
@@ -12,7 +13,12 @@ print(simple_dict)
 field_names = list(simple_dict[0].keys())
 print(field_names)
 
-writer = csv.DictWriter(open('output.csv', 'w', newline='', encoding='utf-8'), field_names, delimiter=';')
+writer = csv.DictWriter(open('output.csv',
+                             'w',
+                             newline='',
+                             encoding='utf-8'),
+                        field_names,
+                        delimiter=';')
 writer.writeheader()
 for r in simple_dict:
     writer.writerow(r)
