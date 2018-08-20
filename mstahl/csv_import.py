a1a06185d69f6549389a08e40675939862fee9e3
Noordsestern <tonison@gmx.de>
2018-08-20 10:17:29 +0200

    csv_import von markus

---
+++ mstahl/csv_import.py
@@ -0,0 +1,15 @@
+seperator = ','
+csv = []
+
+with open("../beispiel.csv") as file:
+    columns = file.readline().strip().split(seperator)
+    # print(columns)
+    for line in file.readlines():
+        values = line.strip().split(seperator)
+        row = {}
+        for i in range(len(values)):
+            row[columns[i]] = values[i]
+        csv.append(row)
+
+for line in csv:
+    print(line)
