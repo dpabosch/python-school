cb7debe0be3066c2ecc12ee66f3602967c378a01
Karsten Wolf <karsten.wolf@postadress.de>
2018-08-20 13:41:13 +0200

    Auto stash before rebase of "origin/master"

--- wolf72/readcsv.py
+++
@@ -1,6 +0,0 @@
-import csv
-with open('../probieren.csv', newline='') as csvfile:
-    csvData = csv.DictReader(csvfile, delimiter=' ', quotechar='|')
-    print(csvData.fieldnames)
-    for row in csvData:
-        print(row)
