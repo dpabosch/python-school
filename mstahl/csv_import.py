05941bdb2474a60d4f91da0d5b56b2ddcf7fc82c
Noordsestern <tonison@gmx.de>
2018-08-20 13:18:36 +0200

    vorhige beispiele in funktionen auslagern

--- mstahl/csv_import.py
+++ mstahl/csv_import.py
@@ -1,33 +1,9 @@
 # coding: utf-8
+import csv
 
-seperator_in = ';'
-csv = []
+inputFile = "../probieren.csv"
+outputFile = "probieren_mit_id.csv"
 
-# Beispieldatei auslesen
-with open("../probieren.csv",encoding="utf-8") as file:
-    columns = file.readline().strip().split(seperator_in)
-    # print(columns)
-    for line in file.readlines():
-        values = line.strip().split(seperator_in)
-        row = {}
-        for i in range(len(values)):
-            row[columns[i]] = values[i]
-        csv.append(row)
+csv_file = csv.csv_import(inputFile)
 
-# Daten schreiben
-# Schreibe vor jede Zeile eine id (aufsteigend) und verwende als serpator ein Komma
-seperator_out = ","
-with open("probien_mit_id.csv",'w',encoding="utf-8") as file:
-    # schreibe header
-    header = "id" + seperator_out + seperator_out.join(csv[0].keys())
-    file.write(header+"\n")
-    # schreibe daten zeilenweise
-    for line in csv:
-        id = str(csv.index(line)+1)
-        out_line = seperator_out.join(line.values())
-        row= id + seperator_out + out_line + "\n"
-        file.writelines(row)
-
-with open("probien_mit_id.csv",encoding="utf-8") as file:
-    for line in file.readlines():
-        print(line.strip())
+csv.csv_export(outputFile,csv_file)
